e6582ca65a404a079a9dc566256adb75d4b2dd8c
ANSHIKA BANG <bang.anshika124@gmail.com>
2022-06-30 23:31:48 +0530

    Add files via upload

---
+++ SALVOS/Scripts/GameManager.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour
+{
+    public Question[] questions;
+    private static List<Question> unansweredQuestions;
+    public Text score;
+    public Text Progress;
+    public static int i = 0;
+    private static int j = 0;
+    public static int scene = 0;
+    private Question currentQuestion;
+    [SerializeField]
+    private Text factText;
+    [SerializeField]
+    private Text opt1;
+    [SerializeField]
+    private Text opt2;
+    [SerializeField]
+    private Text opt3;
+    [SerializeField]
+    private Text opt4;
+    [SerializeField]
+    private float TimeBwQuestions = 1f;
+    [SerializeField]
+    private Animator animator;
+
+    void Start()
+    {
+        scene = 0;
+        score.text = "Score:  " + i.ToString();
+        if (unansweredQuestions == null || unansweredQuestions.Count == 0)
+        {
+            unansweredQuestions = questions.ToList<Question>();
+        }
+        Progress.text = "Progress : " + j.ToString() + "/10";
+        j++;
+        if (j >= 11)
+        {
+            scene = 1;
+            j = 0;
+            SceneManager.LoadScene(7);
+        }
+        SetCurrentQuestion();
+
+    }
+
+    void SetCurrentQuestion()
+    {
+        int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
+        currentQuestion = unansweredQuestions[randomQuestionIndex];
+
+        factText.text = currentQuestion.fact;
+        opt1.text = currentQuestion.option[0];
+        opt2.text = currentQuestion.option[1];
+        opt3.text = currentQuestion.option[2];
+        opt4.text = currentQuestion.option[3];
+
+        unansweredQuestions.RemoveAt(randomQuestionIndex);
+    }
+
+    IEnumerator NextQuestion()
+    {
+        unansweredQuestions.Remove(currentQuestion);
+        yield return new WaitForSeconds(TimeBwQuestions);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+
+    public void Opt0()
+
+    {
+
+        if (currentQuestion.ans == 0)
+        {
+            i++;
+            animator.SetTrigger("T_OP0");
+        }
+        else
+        {
+            animator.SetTrigger("F_OP0");
+        }
+        StartCoroutine(NextQuestion());
+    }
+    public void Opt1()
+    {
+        if (currentQuestion.ans == 1)
+        {
+            i++;
+            animator.SetTrigger("T_OP1");
+        }
+        else
+        {
+            animator.SetTrigger("F_OP1");
+        }
+        StartCoroutine(NextQuestion());
+    }
+    public void Opt2()
+    {
+        if (currentQuestion.ans == 2)
+        {
+            i++;
+            animator.SetTrigger("T_OP2");
+        }
+        else
+        {
+            animator.SetTrigger("F_OP2");
+        }
+        StartCoroutine(NextQuestion());
+    }
+    public void Opt3()
+    {
+        if (currentQuestion.ans == 3)
+        {
+            i++;
+            animator.SetTrigger("T_OP3");
+        }
+        else
+        {
+            animator.SetTrigger("F_OP3");
+        }
+        StartCoroutine(NextQuestion());
+    }
+
+
+}
---
+++ SALVOS/Scripts/GameScript.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public static class hasComponent
+{
+    public static bool HasComponent<T>(this GameObject flag) where T : Component
+    {
+        return flag.GetComponent<T>() != null;
+    }
+}
+public class GameScript : MonoBehaviour
+{
+    public static int priority;
+    public static int scene = 0;
+    public GameObject[] Pox;
+    public Text score;
+    public GameObject submit,p;
+    public Text mt;
+
+    public void gameScore(GameObject[] c, ref int budget, ref int score)
+    {
+        string[] obj = new string[12];
+        obj[0] = "Bank";
+        obj[1] = "Bridge";
+        obj[2] = "Flyover";
+        obj[3] = "Hospital";
+        obj[4] = "Hotel";
+        obj[5] = "House";
+        obj[6] = "Park";
+        obj[7] = "Road";
+        obj[8] = "School";
+        obj[9] = "Shop";
+        obj[10] = "Temple";
+        obj[11] = "Train";
+        int[] price = { 4000, 11000, 7000, 6000, 6000, 1000, 10000, 7000, 3000, 2000, 5000, 9000 };
+        int[] priority = { 2, 1, 1, 3, 1, 3, 1, 3, 3, 2, 0, 1 };
+        for (int i = 0; i < c.Length; i++)
+        {
+            for (int j = 0; j < obj.Length; j++)
+                if (c[i].transform.Find(obj[j]))
+                {
+                    budget -= price[j];
+                    score += priority[j];
+                }
+        }
+
+    }
+
+    public int exit(GameObject[] c)
+    {
+        int check = 1;
+        for (int i = 0; i < c.Length; i++)
+        {
+            if (c[i].transform.childCount == 0)
+            {
+                check = 0;
+            }
+        }
+        return check;
+    }
+
+
+    private void Start()
+    {
+        priority = 0;
+    }
+    // Use this for initialization
+    private void Update()
+    {
+        int budget = 25000;
+        int value = 0;
+        gameScore(Pox, ref budget, ref value);
+        priority = value;
+        score.text = "Budget : " + (budget).ToString() + "   ";
+        if (exit(Pox) == 1)
+        {
+            if (budget >= 0)
+            {
+                submit.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            submit.gameObject.SetActive(false);
+        }
+    }
+    public void finish()
+    {
+        scene = 1;
+        if(priority<=15){
+            mt.text="You lose. HINT: Spend on IMPORTANT things.";
+        }
+        else{
+            mt.text="Congratulations!!\nYou made a right choice. Spend on necessary things only.";
+        }
+        p.SetActive(true);
+    }
+}
